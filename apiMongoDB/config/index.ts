af44d021b24cef918ab9031ba27d3eac7984776f
Tranquocbao2799 <bao.tq01103@sinhvien.hoasen.edu.vn>
2025-02-17 14:19:21 +0700

    test api

---
+++ apiMongoDB/config/index.ts
@@ -0,0 +1,2 @@
+export const MONGO_URI = "mongodb+srv://baotq01103:<MzuB5ZVSlmeqlaYu>@cluster0.h2j6b.mongodb.net/"
+export const PORT = process.env.PORT||9000;
--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { TabsStackScreenProps } from '../Navigation/TabsNavigator';
 import { SafeAreaView } from 'react-native';
 import { HeadersComponent } from '../Components/HeaderComponent/HeaderComponent';
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
 
 type Props = {}
 
@@ -10,9 +11,20 @@ const HomeScreen = ({navigation, route}: TabsStackScreenProps<"Home">) => {
     const gotoCartScreen = () => {
         navigation.navigate("Cart")
     }
+
+    const slider Images = [
+        "https://gomsuhcm.com/wp-content/uploads/2023/12/Tho-tet-2-cau-hai-huoc-cho-nam-2024.jpg", // Network image
+        "https://cdn-media.sforum.vn/storage/app/media/wp-content/uploads/2023/10/tho-chuc-tet-hai-huoc-thumbnail.jpg",
+        "https://i.ytimg.com/vi/OvUolzfolQc/maxresdefault.jpg",
+        "https://bizweb.dktcdn.net/thumb/1024x1024/100/408/770/products/d373f758-620f-4da0-b31e-6c993c0195fe.jpg"
+    ]
     return (
         <SafeAreaView style={{ paddingTop: Platform.OS === "android" ? 40:0, flex: 1, backgroundColor: "black"}}>
             <HeadersComponent gotoCartScreen={gotoCartScreen}/>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}
+                style{{ backgroundColor: "#efg "}}
+            >
+                <ImageSlider images={sliderImages} />
         </SafeAreaView>
     )
 }
